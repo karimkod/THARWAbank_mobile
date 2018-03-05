38622f69756b86a93ff3a83172aa79aed20d6127
karimcode <ea_bournane@esi.dz>
2018-03-05 13:30:51 +0100

    Added fields in sing_up_activity layout and changed from constraint layout to linear layout

--- app/src/main/java/com/tharwa/solid/tharwa/View/LoginActivity.kt
+++ app/src/main/java/com/tharwa/solid/tharwa/View/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.tharwa.solid.tharwa.View
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import com.tharwa.solid.tharwa.R
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        sign_up.setOnClickListener({
+            intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/tharwa/solid/tharwa/View/SignUpActivity.kt
@@ -0,0 +1,30 @@
+package com.tharwa.solid.tharwa.View
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tharwa.solid.tharwa.R
+import com.tharwa.solid.tharwa.TakePictureFragment
+import kotlinx.android.synthetic.main.login_activity.*
+import kotlinx.android.synthetic.main.sign_up_activity.*
+
+
+class SignUpActivity : AppCompatActivity(),TakePictureFragment.OnFragmentInteractionListener
+{
+
+
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_activity)
+        val toolbar = signupToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Inscription"
+
+
+    }
+}
